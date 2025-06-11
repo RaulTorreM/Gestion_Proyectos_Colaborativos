ded02bc5c3420e0e297ab3b0760983df6ddcc744
JosueGarciaBetancourt <garciabetancourtjosuedaniel@gmail.com>
2025-06-11 15:24:18 -0500

    feature backend: Gestión de épicas

--- backend/src/tests/unit/projects/getProjects.test.js
+++ backend/src/tests/unit/projects/getProjects.test.js
@@ -79,7 +79,7 @@ describe('GET /api/projects/:id', () => {
 		expect(response.body.error).toBe('Invalid ID.');
 	});
 
-	// 2. Proyecto no encontrado
+	// 2. Proyecto no encontrad
 	test('should return 404 when project is not found', async () => {
 		mockProjectHelpers.setupFindOneMock(null);
 
