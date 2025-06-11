b8130e2fed97c717762171cc33ebc619b8c0b4ee
JosueGarciaBetancourt <garciabetancourtjosuedaniel@gmail.com>
2025-06-11 15:23:34 -0500

    feature backend: Login

--- backend/src/tests/unit/projects/getProjects.test.js
+++ backend/src/tests/unit/projects/getProjects.test.js
@@ -79,6 +79,7 @@ describe('GET /api/projects/:id', () => {
 		expect(response.body.error).toBe('Invalid ID.');
 	});
 
+	
 	// 2. Proyecto no encontrado
 	test('should return 404 when project is not found', async () => {
 		mockProjectHelpers.setupFindOneMock(null);
